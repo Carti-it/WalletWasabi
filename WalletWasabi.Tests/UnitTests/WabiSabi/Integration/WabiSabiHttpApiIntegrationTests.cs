39b88db6a66f7bb819461061222f4d602cf0ef61
Carti <carti_it@proton.me>
2026-08-17 14:49:12 +0200

    rpc

--- WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
@@ -569,7 +569,7 @@ public class WabiSabiHttpApiIntegrationTests : IClassFixture<WabiSabiApiApplicat
 	public class TestableRpcClient : RpcClientBase
 	{
 		public TestableRpcClient(RpcClientBase rpc)
-			: base(rpc.Rpc)
+			: base(rpc.RpcClient)
 		{
 		}
 
