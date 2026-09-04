c5262d7ba0f70641790e5f5b481136a3115bb24e
Carti <carti_it@proton.me>
2026-09-05 00:02:20 +0200

    ByteHelpers: Remove unused `Combine` method

--- WalletWasabi/Helpers/ByteHelpers.cs
+++ WalletWasabi/Helpers/ByteHelpers.cs
@@ -2,22 +2,6 @@ namespace WalletWasabi.Helpers;
 
 public static class ByteHelpers
 {
-	// https://stackoverflow.com/questions/415291/best-way-to-combine-two-or-more-byte-arrays-in-c-sharp
-	/// <summary>
-	/// Fastest byte array concatenation in C#
-	/// </summary>
-	public static byte[] Combine(params byte[][] arrays)
-	{
-		byte[] ret = new byte[arrays.Sum(x => x.Length)];
-		int offset = 0;
-		foreach (byte[] data in arrays)
-		{
-			Buffer.BlockCopy(data, 0, ret, offset, data.Length);
-			offset += data.Length;
-		}
-		return ret;
-	}
-
 	public static bool CompareFast(byte[]? array1, byte[]? array2)
 	{
 		if (array1 == array2)
